0b17dd846714ec3645833c152bd21150d66b902f
davnils <nilsson.dd+github+gravatarobscurity@gmail.com>
2013-01-24 22:56:44 +0100

    Useful example proof demonstrating equivalence of subroutines.

--- Test.hs
+++ Test.hs
@@ -13,27 +13,52 @@ import Data.SBV
 import Shared
 import Verification
 
+-- Two versions of a program that doubles an integer in 'r1' a total of 'r2' times.
+-- Proofs are currently done over the register file, hence stack etc is ignored.
+-- Using GHC interpreter:
+--
+--  \ :l Test.hs
+--  [3 of 3] Compiling Main             ( Test.hs, interpreted )
+--  Ok, modules loaded: Main, Shared, Verification.
+--  \ proveEquivalence prog1 prog2
+--  Q.E.D.
+--
+
 prog1 = do
   label "loop"
-  subi R1 32
-  subi R2 64
+  rcall "addReg"
+  subi R2 1
   brne "loop"
-  subi R2 64
+  rjmp "done"
+
+  label "addReg"
+  add R1 R1
+  ret
+
+  label "done"
 
 prog2 = do
-  subi R1 128
+  label "loop"
+  lsl R1
+  subi R2 1
+  brne "loop"
 
-preserveRegisters = do
+  label "done"
+
+freeRegisters = do
   let regs = registers initialState
   r1 <- sWord8 "r1"
-  let arr' = regs // [(R1, r1)]
+  r2 <- sWord8 "r2"
+  let arr' = regs // [(R1, r1), (R2, r2)]
   return $ initialState { registers = arr' }
 
-verify :: Symbolic SBool
-verify = do
-  state <- preserveRegisters
-  let eval prog = registers $ program prog state
-  return $ (eval prog1 ! R1) .== (eval prog2 ! R1)
+proveEquivalence :: AVR -> AVR -> IO ThmResult
+proveEquivalence proga progb = prove statement
+  where
+  statement = do
+    state <- freeRegisters
+    let eval prog = registers $ program prog state
+    return $ (eval proga ! R1) .== (eval progb ! R1)
 
 main :: IO ()
-main = prove verify >>= print
+main = proveEquivalence prog1 prog2 >>= print
